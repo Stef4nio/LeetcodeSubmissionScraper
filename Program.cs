67f46d45f359b7e4372f2c290b26917dc29e04b7
stef4nio <stef4nio@gmail.com>
2020-09-11 00:26:25 +0300

    Removed Login button due to reCaptcha limitations. Fixed removing HTML tags from txt file. Added new feature. Now you cah choose whether to download all your submissions or just one for each problem. Resolved problem with page requirment to reload it. Changed code downloading behaviour. Now a new page opens only once and being reused, instead of being constanly closed and opened.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -79,38 +80,9 @@ namespace LeetcodeSubmissionScraper
             Console.WriteLine("Unzipping successful");
         }
         
-        private static void Login(string username, string password, ChromeDriver cd)
-        {
-            cd.Url = @"https://leetcode.com/submissions/#/1";
-            cd.Navigate();
-            IWebElement e = cd.FindElementById("id_login");
-            e.SendKeys(username);
-            Console.WriteLine("Login entered successfully");
-            e = cd.FindElementById("id_password");
-            e.SendKeys(password);
-            Console.WriteLine("Password entered successfully");
-
-            var signInButtonUnlockWait = new WebDriverWait(cd, TimeSpan.FromSeconds(5));
-            signInButtonUnlockWait.Until(driver =>
-            {
-                try
-                {
-                    driver.FindElement(By.Id("initial-loading"));
-                    return false;
-                }
-                catch (NoSuchElementException)
-                {
-                    Console.WriteLine("Preloader destroyed, ready for sign in");
-                    return true;
-                }
-            });
-            e = cd.FindElementById("signin_btn");
-            Console.WriteLine("Attempting sign in");
-            e.Click();
-        }
-
         private static IWebElement GetNextButton(ChromeDriver cd)
         {
+            WaitForRecaptchaBan(cd,2000);
             var navButtonsContainerWait = new WebDriverWait(cd, TimeSpan.FromSeconds(5));
             navButtonsContainerWait.Until(driver => driver.FindElement(By.CssSelector(".pager")).Displayed);
             Console.WriteLine("Navigation buttons container detected successfully");
@@ -122,46 +94,77 @@ namespace LeetcodeSubmissionScraper
 
         static (string,string) DownloadCodeFromPage(string submissionUrl, ChromeDriver cd)
         {
-            Console.WriteLine("Open " + submissionUrl + " in a new tab");
-            ((IJavaScriptExecutor)cd).ExecuteScript("window.open();");
-            cd.SwitchTo().Window(cd.WindowHandles.Last());
             cd.Navigate().GoToUrl(submissionUrl);
-            Console.WriteLine("Opened new tab successfully");
             var codeBlockWait = new WebDriverWait(cd, TimeSpan.FromSeconds(5));
             codeBlockWait.Until(driver => driver.FindElement(By.ClassName("ace_content")).Displayed);
             IWebElement problemLink = cd.FindElementByCssSelector("a.inline-wrap");
             string problemName = problemLink.Text;
             Console.WriteLine("Current problem name: "+problemName);
-            Thread.Sleep(500);
             IWebElement codeBlock = cd.FindElementByClassName("ace_content");
             Console.WriteLine("Code block captured. Downloading...");
             string codeData = codeBlock.FindElement(By.CssSelector("div.ace_layer.ace_text-layer")).GetAttribute("innerHTML");
             Console.WriteLine("Successfully downloaded code data");
-            Console.WriteLine("Closing tab: " + submissionUrl);
-            cd.Close();
-            cd.SwitchTo().Window(cd.WindowHandles[0]);
-            Console.WriteLine("Tab closed successfully");
             return (problemName,codeData);
         }
 
+        static void WaitForSubmissionsTable(ChromeDriver cd, int milliSeconds)
+        { 
+            WaitForRecaptchaBan(cd,1000);
+            Console.WriteLine("Attempt to load submissions page");
+            var submissionsTableWait = new WebDriverWait(cd, TimeSpan.FromMilliseconds(milliSeconds));
+            submissionsTableWait.Until(driver => driver.FindElement(By.CssSelector(".table")).Displayed);
+            Console.WriteLine("Submissions page loaded. Sign in successful");
+        }
+
+        static void WaitForRecaptchaBan(ChromeDriver cd, int milliSeconds)
+        {
+            var captchaCrashWait = new WebDriverWait(cd, TimeSpan.FromMilliseconds(milliSeconds));
+            try
+            {
+                captchaCrashWait.Until(driver =>
+                    driver.FindElement(By.ClassName("noty_body")).Displayed);
+                Thread.Sleep(3000);
+                cd.Navigate().Refresh();
+                WaitForSubmissionsTable(cd,60000);
+            }
+            catch (WebDriverTimeoutException e)
+            {
+                Console.WriteLine("No reload required, continuing");
+            }
+        }
+
+        static void OpenNewTab(ChromeDriver cd)
+        {
+            Console.WriteLine("Opening a new tab");
+            ((IJavaScriptExecutor)cd).ExecuteScript("window.open();");
+            cd.SwitchTo().Window(cd.WindowHandles.Last());
+            Console.WriteLine("Opened new tab successfully");
+        }
+
+        static void CloseCurrentTab(ChromeDriver cd)
+        {
+            Console.WriteLine("Closing tab: " + cd.Url);
+            cd.Close();
+            cd.SwitchTo().Window(cd.WindowHandles[0]);
+            Console.WriteLine("Tab closed successfully");
+        }
+        
         static void RemoveRedunduntDataFromCode(ref string code)
         {
             code = code.Replace(
-                "<div class=\"ace_line_group\" style=\"height:17px\"><div class=\"ace_line\" style=\"height:17px\">",
-                "");
+                "<div class=\"ace_line_group\" style=\"height:17px\"><div class=\"ace_line\"","");
+            code = Regex.Replace(code, " style=\"height:\\d{1,4}px\">", " ");
             code = code.Replace("</div></div>", "\n");
         }
-        static void DownloadSolutions(ChromeDriver cd)
+        
+        //downloadType: 1 - all submissions, 2 - one for each problem
+        static void DownloadSolutions(ChromeDriver cd,int downloadType)
         {
             if (!Directory.Exists(SOLUTION_FOLDER))
             {
                 Directory.CreateDirectory(SOLUTION_FOLDER);
             }
-            
-            Console.WriteLine("Attempt to load submissions page");
-            var submissionsTableWait = new WebDriverWait(cd, TimeSpan.FromSeconds(10));
-            submissionsTableWait.Until(driver => driver.FindElement(By.CssSelector(".table")).Displayed);
-            Console.WriteLine("Submissions page loaded. Sign in successful");
+            WaitForSubmissionsTable(cd, 600000);
             IWebElement nextButton = GetNextButton(cd);
             List<string> submissionsToDownload = new List<string>();
             while (!nextButton.GetAttribute("class").Contains("disabled"))
@@ -173,17 +176,22 @@ namespace LeetcodeSubmissionScraper
                 submissionsToDownload.AddRange(acceptedSubmissionsButtons.Select(button => button.GetAttribute("href")).ToList());
                 cd.Navigate().GoToUrl(nextButton.FindElement(By.XPath("a")).GetAttribute("href"));
                 nextButton = GetNextButton(cd);
-                Thread.Sleep(1000);
             }
 
             Console.WriteLine($"\"Next\" button is Disabled, commencing saving sequence");
             Console.WriteLine($"Detected {submissionsToDownload.Count} accepted submissions");
             
             Dictionary<string,int> problemSolutionsAmount = new Dictionary<string, int>();
+            OpenNewTab(cd);
             for(int i = 0;i<submissionsToDownload.Count;i++)
             {
+                string submissionUrl = submissionsToDownload[i];
+                
                 Console.Write($"[{i+1}/{submissionsToDownload.Count}] ");
                 string problemName, codeData;
+                
+                
+                
                 (problemName, codeData) = DownloadCodeFromPage(submissionsToDownload[i], cd);
                 if (!problemSolutionsAmount.ContainsKey(problemName))
                 {
@@ -193,7 +201,25 @@ namespace LeetcodeSubmissionScraper
                 {
                     problemSolutionsAmount[problemName]++;
                 }
-                string filename = $"{SOLUTION_FOLDER}/{problemName} v{problemSolutionsAmount[problemName]}.txt".Replace(' ','_');
+
+                string filename = "";
+                
+                if (downloadType == 1)
+                {
+                    filename =
+                        $"{SOLUTION_FOLDER}/{problemName} v{problemSolutionsAmount[problemName]}.txt".Replace(' ', '_');
+                }
+                else if(problemSolutionsAmount[problemName]==1)
+                {
+                    filename =
+                        $"{SOLUTION_FOLDER}/{problemName}.txt".Replace(' ', '_');
+                }
+                else
+                {
+                    Console.WriteLine("Solution to this problem has been already downloaded. Skipping...");
+                    continue;
+                }
+
                 Console.WriteLine($"Attempt to save solution of {problemName} at:\n{filename}");
                 RemoveRedunduntDataFromCode(ref codeData);
                 try
@@ -208,30 +234,39 @@ namespace LeetcodeSubmissionScraper
                     throw;
                 }
             }
-            
+
+            CloseCurrentTab(cd);
+
         }
         
         public static void Main(string[] args)
         {
-            if (!File.Exists("Credentials.txt"))
+            if (!Directory.Exists("chromedriver_win32"))
             {
                 Console.WriteLine("First time login detected. Downloading additional files...");
                 Console.WriteLine("Getting your Google Chrome version...");
                 string version = GetGoogleChromeVersion();
                 Console.WriteLine("Your Google Chrome version is: "+version);
                 DownloadChromeDriver(version);
-                File.WriteAllText("Credentials.txt","<username> <password>");
-                Console.WriteLine("Enter your credentials to Credentials.txt that was just created and restart the program");
             }
-            else
+            Console.Write("Do you want to download ALL submissions or just ONE for each problem? Enter 1 or 2: ");
+            string answer = Console.ReadLine();
+            int result;
+            try
             {
-                string credentials = File.ReadAllText("Credentials.txt");
-                ChromeDriver cd = new ChromeDriver(@"chromedriver_win32");
-                Login(credentials.Split(' ')[0],credentials.Split(' ')[1],cd);
-                DownloadSolutions(cd);
-                Console.WriteLine("Scrape completed successfully!!!");
-                cd.Close();
+                result = Int32.Parse(answer);
             }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Invalid input. Shutting down.");
+                throw;
+            }
+            ChromeDriver cd = new ChromeDriver(@"chromedriver_win32");
+            cd.Url = @"https://leetcode.com/submissions/#/1";
+            cd.Navigate();
+            DownloadSolutions(cd,result);
+            Console.WriteLine("Scrape completed successfully!!!");
+            cd.Close();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
